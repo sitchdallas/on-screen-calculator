8efcf7470b13bdbb7ba933c743f8a645f9a475a7
sitchdallas <125512025+sitchdallas@users.noreply.github.com>
2023-03-28 17:32:43 -0500

    added styling and HTML

---
+++ js-file.js
@@ -0,0 +1,35 @@
+let displayValue = '0';
+let firstInputValue = null;
+let secondInputValue = null;
+let firstOperator = null;
+let secondOperator = null;
+let result = null;
+
+/*
+function add() {
+    displayValue = newDisplayValue;
+    newCalculation = ((displayValue) - (userInputValue));
+    newDisplayValue = newCalculation;
+}
+
+function subtract() {
+    displayValue = newDisplayValue;
+    newCalculation = ((displayValue) - (userInputValue));
+    newDisplayValue = newCalculation;
+}
+
+function multiply() {
+    displayValue = newDisplayValue;
+    newCalculation = ((displayValue) * (userInputValue));
+    newDisplayValue = newCalculation;
+}
+
+function divide() {
+    displayValue = newDisplayValue;
+    newCalculation = ((displayValue) / (userInputValue));
+    newDisplayValue = newCalculation;
+} */
+
+function clearDisplay() {
+    
+}
