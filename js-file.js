2cb3ab17f336a01cfba4969b6e31d50c91469af0
sitchdallas <125512025+sitchdallas@users.noreply.github.com>
2023-03-28 21:53:20 -0500

    added function to display numbers that were pressed

--- js-file.js
+++ js-file.js
@@ -4,6 +4,30 @@ let secondInputValue = null;
 let firstOperator = null;
 let secondOperator = null;
 let result = null;
+let display = '0';
+
+/*
+find a way to touch numbers on the screen and add it to the displayValue 
+
+find a way to compute it all, for example..
+if you press the "+" button, then put the first and second value through a math.add(x, y)
+
+*/
+
+
+//display numbers on the screen
+let operators = document.querySelectorAll(".operator");
+display = document.getElementById("display")
+
+for (let i = 0; i < operators.length; i++) {
+    operators[i].addEventListener("click", function() {
+      display.innerHTML += this.innerText;
+    });
+  };
+
+//function for picking which operand to use
+let operands = document.querySelectorAll(".operands");
+
 
 /*
 function add() {
@@ -30,6 +54,6 @@ function divide() {
     newDisplayValue = newCalculation;
 } */
 
-function clearDisplay() {
-    
-}
+
+
+
